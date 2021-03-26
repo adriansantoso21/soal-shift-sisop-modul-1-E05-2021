5fbcc3c059deb7bbb0c3c1cb3831cc0fa847d34a
nurhidayati0901 <hidanur0901@gmail.com>
2021-03-26 22:37:19 +0700

    Submit soal 1

---
+++ soal1/soal1.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+INPUT="syslog.log"
+
+# 1a
+regex_type="(INFO|ERROR)"
+regex_msgs="(?<=[INFO|ERROR] ).*(?<=\ )"
+regex_err_msgs="(?<=ERROR ).*(?<=\ )"
+regex_info_msgs="(?<=INFO ).*(?<=\ )"
+regex_username="(?<=\().*(?=\))"
+
+type=$(grep -oP "$regex_type" "$INPUT")
+err_msgs=$(grep -oP "$regex_err_msgs" "$INPUT")
+info_msgs=$(grep -oP "$regex_info_msg" "$INPUT")
+msgs=$(grep -oP "$regex_msgs" "$INPUT")
+username=$(grep -oP "$regex_username" "$INPUT")
+
+# 1b
+uniq_err_msgs=$(grep -oP "$regex_err_msgs" "$INPUT" | sort | uniq -c | sort -nr)
+
+# 1c
+regex_err="(?<=ERROR ).*(?<=\))"
+regex_info="(?<=INFO ).*(?<=\))"
+err=$(grep -oP "$regex_err" "$INPUT")
+info=$(grep -oP "$regex_info" "$INPUT")
+
+# 1d
+printf "ERROR,COUNT\n" > "error_message.csv"
+grep -oP "$regex_err_msgs" "$INPUT" | sort | uniq -c | sort -nr | while read count msgs;
+do
+        printf "%s,%d\n" "$msgs" "$count" >> "error_message.csv"
+done
+
+# 1e
+printf "USERNAME,INFO,ERROR\n" > "user_statistic.csv"
+grep -oP "$regex_username" "$INPUT" | sort | uniq | while read user;
+do
+        n_info=$(grep "$user" <<< "$info" | wc -l);
+        n_error=$(grep "$user" <<< "$err" | wc -l);
+      	 printf "%s,%d,%d\n" "$user" "$n_info" "$n_error" >> "user_statistic.csv"
+done
+
