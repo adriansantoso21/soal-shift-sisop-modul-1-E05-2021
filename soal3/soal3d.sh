2e2e6a124694d6a37840392ede935605ffe01431
Oni Ahmad <46712776+oni4hmad@users.noreply.github.com>
2021-04-02 10:40:21 +0700

    Update soal3d.sh

--- soal3/soal3d.sh
+++ soal3/soal3d.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-cd /home/adr01/Documents/SesiLab1/SoalPraktikum/Soal3
+
+cd /media/sf_soal-shift-sisop-modul-1-E05-2021/soal3/
+
 tanggal="$(date '+%m%d%Y')"
 zip -P $tanggal -rm Koleksi.zip */
